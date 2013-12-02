37922b82c5fc9ed4292558267d3720e1b68a96e0
David Tudury <david.tudury@voxer.com>
2013-12-02 14:01:40 -0800

    cleanup code a bit (IMHO) [ 0.2.3 ]

--- index.js
+++ index.js
@@ -1,83 +1,62 @@
-module.exports = exports = is;
-
-
-function _makeType(obj) {
-    var typeString = {}.toString.call(obj);
-    var typeName = typeString.match(/\[object (.*)\]/i)[1];
-    var sameAs = function(type) {
-        if (is(type).an.Array) return !!~type.indexOf(sameAs);
-        return typeString === type.toType();
-    };
-    var notSameAs = function(type) {
-        return !sameAs(type);
-    };
-    sameAs.toType = function() {
-        return typeString;
-    }
-    sameAs.toString = function() {
-        return typeName;
-    }
-    sameAs.is = sameAs.a = sameAs.an = notSameAs.not = sameAs;
-    notSameAs.is = notSameAs.a = notSameAs.an = sameAs.not = notSameAs;
-    return sameAs;
-}
-
-
-var ARGUMENTS = exports.ARGUMENTS = _makeType((function () {return arguments;})());
-var ARRAY = exports.ARRAY = _makeType([]);
-var BOOLEAN = exports.BOOLEAN = _makeType(true);
-var DATE = exports.DATE = _makeType(new Date);
-var FUNCTION = exports.FUNCTION = _makeType(function () {});
-var MATH = exports.MATH = _makeType(Math);
-var NULL = exports.NULL = _makeType(null);
-var NUMBER = exports.NUMBER = _makeType(0);
-var OBJECT = exports.OBJECT = _makeType({});
-var REGEXP = exports.REGEXP = _makeType(/./);
-var STRING = exports.STRING = _makeType("");
-var UNDEFINED = exports.UNDEFINED = _makeType();
-
-
-var BUILTIN_TYPE = [ARGUMENTS, ARRAY, BOOLEAN, DATE, FUNCTION, MATH, NULL, NUMBER, OBJECT, REGEXP, STRING, UNDEFINED];
-var JSON_BASIC_TYPE = [ARRAY, BOOLEAN, NULL, NUMBER, OBJECT, STRING, UNDEFINED];
-var CORE_OBJECT = [ARRAY, BOOLEAN, DATE, FUNCTION, MATH, NUMBER, REGEXP, STRING];
-var PRIMITIVE = [BOOLEAN, NULL, NUMBER, STRING, UNDEFINED];
-
-
-var _typeSets = {BuiltinType: BUILTIN_TYPE, JsonBasicType: JSON_BASIC_TYPE, CoreObject: CORE_OBJECT, Primitive: PRIMITIVE};
-
-
 var _typeMap = {};
-for(var i = 0; i < BUILTIN_TYPE.length; i++) {
-    var type = BUILTIN_TYPE[i];
-    _typeMap[type.toType()] = type;
-}
-for(var i = 0; i < BUILTIN_TYPE.length; i++) {
-    var type = BUILTIN_TYPE[i];
-    for(var j = 0; j < BUILTIN_TYPE.length; j++) {
-        var otherType = BUILTIN_TYPE[j];
-        type[otherType.toString()] = type.is(otherType);
-        type.not[otherType.toString()] = type.is.not(otherType);
-    }
-}
-for(var i = 0; i < BUILTIN_TYPE.length; i++) {
-    var type = BUILTIN_TYPE[i];
-    for(var typeSetName in _typeSets) {
-        var typeSet = _typeSets[typeSetName];
-        type[typeSetName] = type.is(typeSet);
-        type.not[typeSetName] = type.is.not(typeSet);
-    }
-}
-for(var typeSetName in _typeSets) {
-    var typeSet = _typeSets[typeSetName];
-    var message = typeSet.slice(0, typeSet.length - 1).join(", ") + ", or " + typeSet[typeSet.length - 1];
-    typeSet.toString = (function (message) {
-        return function() {
-            return message;
-        }
-    })(message);
-}
 
-
-function is(obj) {
+var is = module.exports = exports = function (obj) {
     return _typeMap[{}.toString.call(obj)];
+};
+
+function _make(obj) {
+    var thisType = {}.toString.call(obj);
+
+    var thisIs = _typeMap[thisType] = function (other) {
+        return (is(other).an.Array) ? !!~other.indexOf(thisIs) : thisIs === other;
+    };
+    thisIs.not = function (other) {return !thisIs(other)};
+
+    var thisName = thisType.match(/\[object (.*)\]/i)[1];
+    thisIs.toString = function () {return thisName};
+    thisIs.toType = function () {return thisType};
+
+    thisIs.is = thisIs.a = thisIs.an = thisIs.not.not = thisIs;
+    thisIs.not.is = thisIs.not.a = thisIs.not.an = thisIs.not;
+
+    return thisIs;
+}
+
+var ARGUMENTS  = exports.ARGUMENTS = _make((function () {return arguments;})());
+var ARRAY      = exports.ARRAY =     _make([]);
+var BOOLEAN    = exports.BOOLEAN =   _make(false);
+var DATE       = exports.DATE =      _make(new Date);
+var FUNCTION   = exports.FUNCTION =  _make(function () {});
+var MATH       = exports.MATH =      _make(Math);
+var NULL       = exports.NULL =      _make(null);
+var NUMBER     = exports.NUMBER =    _make(0);
+var OBJECT     = exports.OBJECT =    _make({});
+var REGEXP     = exports.REGEXP =    _make(/./);
+var STRING     = exports.STRING =    _make("");
+var UNDEFINED  = exports.UNDEFINED = _make();
+
+exports.BUILTIN_TYPE    = [ARGUMENTS, ARRAY, BOOLEAN, DATE, FUNCTION, MATH, NULL, NUMBER, OBJECT, REGEXP, STRING, UNDEFINED];
+exports.JSON_BASIC_TYPE = [           ARRAY, BOOLEAN,                       NULL, NUMBER, OBJECT,         STRING, UNDEFINED];
+exports.CORE_OBJECT     = [           ARRAY, BOOLEAN, DATE, FUNCTION, MATH, NUMBER,               REGEXP, STRING           ];
+exports.PRIMITIVE       = [                  BOOLEAN,                       NULL, NUMBER,                 STRING, UNDEFINED];
+
+var _typeSets = {BuiltinType: exports.BUILTIN_TYPE, JsonBasicType: exports.JSON_BASIC_TYPE, CoreObject: exports.CORE_OBJECT, Primitive: exports.PRIMITIVE};
+
+for(var i = 0; i < _typeSets.BuiltinType.length; i++) {
+    var a = _typeSets.BuiltinType[i];
+    for(var j = 0; j < _typeSets.BuiltinType.length; j++) {
+        var b = _typeSets.BuiltinType[j].toString();
+        a.not[b] = !(a[b] = i === j);
+    }
+}
+
+for (var name in _typeSets) {
+    (function (name) {
+        var typeSet = _typeSets[name];
+        typeSet.toString = function() {return typeSet.join(' or ')};
+        for(var i = 0; i < _typeSets.BuiltinType.length; i++) {
+            var type = _typeSets.BuiltinType[i];
+            type[name] = !(type.not[name] = !~typeSet.indexOf(type));
+        }
+    })(name);
 }
